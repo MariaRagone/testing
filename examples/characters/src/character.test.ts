0ea52fbed16f76a6960c0ff389f4e3bad7059567
Maria Ragone <maria@ragone.net>
2025-04-01 12:52:42 -0400

    working on character tests

--- examples/characters/src/character.test.ts
+++ examples/characters/src/character.test.ts
@@ -2,13 +2,30 @@ import { describe, it, expect } from 'vitest';
 import { Character } from './character.js';
 import { Person } from './person.js';
 
+const firstName = 'Ada';
+const lastName = 'Lovelace';
+const role = 'Computer Scientest';
+const character = new Character(firstName, lastName, role);
 describe('Character', () => {
-  it(
-    'should create a character with a first name, last name, and role',
-    () => {},
-  );
+  it('should create a character with a first name, last name, and role', () => {
+    expect(character).toEqual(
+      expect.objectContaining({
+        firstName,
+        lastName,
+        role,
+      }),
+    );
+  });
 
-  it.todo('should allow you to increase the level', () => {});
+  it('should allow you to increase the level', () => {
+    character.levelUp();
+    expect(character.level).toBe(2);
+  });
 
-  it.todo('should update the last modified date when leveling up', () => {});
+  it.todo('should update the last modified date when leveling up', () => {
+    character.levelUp();
+    const initialLastModified = character.lastModified;
+
+    expect(character.lastModified).not.toBe(initialLastModified);
+  });
 });
