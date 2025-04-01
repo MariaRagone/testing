b0919c6a19af3b83b054bf2e859ba02ddc49ca03
mragone <maria.ragone@samaritanministries.org>
2025-04-01 08:37:28 -0400

    artithmetic tests tdd

--- examples/basic-math/src/arithmetic.js
+++ examples/basic-math/src/arithmetic.js
@@ -1,6 +1,10 @@
-export const add = () => {};
+export const add = (a, b) => {
+return a + b;
+};
 
-export const subtract = () => {};
+export const subtract = (a,b) => {
+    return a - b;
+};
 
 export const multiply = () => {};
 
--- examples/basic-math/src/arithmetic.test.js
+++ examples/basic-math/src/arithmetic.test.js
@@ -1,8 +1,17 @@
 import { describe, it, expect } from 'vitest';
+import { add, subtract, multiply, divide } from './arithmetic';
 
-describe.todo('add', () => {});
+describe('add', () => {
 
-describe.todo('subtract', () => {});
+    it('should add two positive numbers', () => {
+        expect(add(2,4)).toBe(6)
+});
+})
+describe('subtract', () => {
+    it('should subtract a number from another number', () => {
+        expect(subtract(6,3)).toBe(3)
+    })
+});
 
 describe.todo('multiply', () => {});
 
