dd19ef3b3ee35d937c889520582838e1eef883d1
mragone <maria.ragone@samaritanministries.org>
2025-04-01 09:32:43 -0400

    added some edge cases for strings

--- examples/basic-math/src/arithmetic.js
+++ examples/basic-math/src/arithmetic.js
@@ -1,4 +1,9 @@
 export const add = (a, b) => {
+    if(typeof a === "string") a = Number(a);
+    if(typeof b === 'string') b = Number(b);
+    if(isNaN(a) || isNaN(b)) throw new Error('Please input numbers.');
+    
+
 return a + b;
 };
 
--- examples/basic-math/src/arithmetic.test.js
+++ examples/basic-math/src/arithmetic.test.js
@@ -2,11 +2,24 @@ import { describe, it, expect } from 'vitest';
 import { add, subtract, multiply, divide } from './arithmetic';
 
 describe('add', () => {
-
+    
     it('should add two positive numbers', () => {
         expect(add(2,4)).toBe(6)
-});
+
+    });
+    it('should parse strings into numbers', () => {
+        expect(add("1","1")).toBe(2) 
+    });
+
+    it('should yell at you if the second argument is a word', () => {
+        expect(() =>(add(2, "potato"))).toThrow('Please input numbers') 
+    });
+
+    it('should yell at you if the first argument is a word', () => {
+        expect(() =>(add("potato", 2))).toThrow() 
+    });
 })
+
 describe('subtract', () => {
     it('should subtract a number from another number', () => {
         expect(subtract(6,3)).toBe(3)
