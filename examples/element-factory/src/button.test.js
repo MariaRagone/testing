ee32ae8dfada329db094e9d512e3d6b0a2aefa3a
Maria Ragone <maria@ragone.net>
2025-04-01 13:36:38 -0400

    started button tests

--- examples/element-factory/src/button.test.js
+++ examples/element-factory/src/button.test.js
@@ -1,9 +1,22 @@
 import { createButton } from './button.js';
+import { screen } from '@testing-library/dom';
 
-describe.todo('createButton', () => {
-  it('should create a button element', () => {});
+describe('createButton', () => {
+  it('should create a button element', () => {
+    document.body.appendChild(createButton());
 
-  it('should have the text "Click Me"', () => {});
+    const button = screen.getByRole('button', { name: 'Click Me' });
+    expect(button).toBeInTheDocument();
+  });
 
-  it('should change the text to "Clicked!" when clicked', async () => {});
+  it('should have the text "Click Me"', () => {
+    const button = createButton();
+    expect(button.textContent).toBe('Click Me');
+  });
+
+  it('should change the text to "Clicked!" when clicked', async () => {
+    const button = createButton();
+    button.click();
+    expect(button.textContent).toBe('Clicked!');
+  });
 });
